e32406a5b22dfdd62dc1c2bc75c8ae4d84f63243
Sarabjeet Singh <sarabjeet.chowdhary@gmail.com>
2022-03-09 19:11:55 +0530

    Fixed a typo in command path. Removed unnecessary sudo invocations. Added exit stements in case of errors.
    Changed Raspbian version from bullseye to buster.

--- setup_build_env.sh
+++ setup_build_env.sh
@@ -10,7 +10,7 @@ _CP=/usr/bin/cp
 _MOUNT=/usr/bin/mount
 _CHROOT=/usr/sbin/chroot
 _DEBOOTSTRAP=/usr/sbin/debootstrap
-_CAT=/usrin/cat
+_CAT=/usr/bin/cat
 
 _EXIT="exit 1"
 
@@ -23,7 +23,7 @@ _CHROOT_DIR=${1}
 
 ${_ECHO} "Installing dependencies"
 
-for name in "qemu-user-static debootstrap chroot"
+for name in "qemu-user-static debootstrap"
 do
     ${_INSTALL} ${name} || ${_EXIT}
 done
@@ -36,7 +36,7 @@ ${_ECHO} "Dependencies installation done."
 #####################################################################
 
 ${_ECHO} "Creating root FS"
-${_DEBOOTSTRAP} --no-check-gpg --foreign --arch=armhf bullseye ${_CHROOT_DIR} http://archive.raspbian.org/raspbian
+${_DEBOOTSTRAP} --no-check-gpg --foreign --arch=armhf buster ${_CHROOT_DIR} http://archive.raspbian.org/raspbian || ${_EXIT}
 
 ${_ECHO} "Copying QEMU static binary"
 cp /usr/bin/qemu-arm-static ${_CHROOT_DIR}/usr/bin
@@ -46,21 +46,21 @@ if [ -f /proc/sys/fs/binfmt_misc/arm ]
 then
     ${_ECHO} "ARM binary format already registered"
 else
-    sudo ${_ECHO} ':arm:M::\x7fELF\x01\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x28\x00:\xff\xff\xff\xff\xff\xff\xff\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff\xff:/usr/bin/qemu-arm-static:' >> /proc/sys/fs/binfmt_misc/register
+    sudo ${_ECHO} ':arm:M::\x7fELF\x01\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x28\x00:\xff\xff\xff\xff\xff\xff\xff\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff\xff:/usr/bin/qemu-arm-static:' >> /proc/sys/fs/binfmt_misc/register || ${_EXIT}
 	${_ECHO} "ARM binary format registered"
 fi
 
 ${_ECHO} "Running debootstrap second stage"
-${_SUDO} ${_CHROOT} ${_CHROOT_DIR} /debootstrap/debootstrap --second-stage
+${_CHROOT} ${_CHROOT_DIR} /debootstrap/debootstrap --second-stage || ${_EXIT}
 
 ${_ECHO} "Setting up package management in guest environment"
-${_SUDO} ${_CAT} > ${_CHROOT_DIR}/etc/apt/sources.list << EOF
-deb http://mirrordirector.raspbian.org/raspbian/ bullseye main contrib non-free rpi
-deb http://archive.raspberrypi.org/debian/ bullseye main
+${_CAT} > ${_CHROOT_DIR}/etc/apt/sources.list << EOF
+deb http://mirrordirector.raspbian.org/raspbian/ buster main contrib non-free rpi
+deb http://archive.raspberrypi.org/debian/ buster main
 EOF
 
-${_SUDO} ${_CHROOT} ${_CHROOT_DIR} apt-key adv --keyserver keyserver.ubuntu.com --recv 82B129927FA3303E
-${_SUDO} ${_CHROOT} ${_CHROOT_DIR} apt-get update
+${_CHROOT} ${_CHROOT_DIR} apt-key adv --keyserver keyserver.ubuntu.com --recv 82B129927FA3303E || ${_EXIT}
+${_CHROOT} ${_CHROOT_DIR} apt-get update || ${_EXIT}
 
 ${_ECHO} "Guest chroot environment is ready at ${_CHROOT_DIR}"
 
